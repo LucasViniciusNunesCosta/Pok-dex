3e04e129e2b410bdfa17f0f3193d025a17d236c0
Lucas Costa <lucasvinicius3105@gmail.com>
2023-04-04 22:47:44 -0300

    :feat: sessao buscar pokemon

---
+++ src/app/Components/search-pokemon/search-pokemon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'searchPokemon',
+  templateUrl: './search-pokemon.component.html',
+  styleUrls: ['./search-pokemon.component.css']
+})
+export class SearchPokemonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
