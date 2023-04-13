8b9e2a2d7421b1b9d1c2dd59af3f179503f156d5
Lucas Costa <lucasvinicius3105@gmail.com>
2023-04-12 23:51:32 -0300

    :feat: criando rota para card detalhe pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewPokemonComponent } from './Components/view-pokemon/view-pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'viewPokemon', component: ViewPokemonComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
